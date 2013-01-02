edc5254cc603fe7a9c6b558d7b421b9bd669fce1
Yahel Carmon <yahelc@gmail.com>
2013-01-02 11:07:53 -0500

    Formatting js

--- twig.js
+++ twig.js
@@ -1,65 +1,67 @@
-function error(){
-	$("pre").css("color","red");
+function error() {
+	$("pre").css("color", "red");
 }
-$(function(){
-	
-$("form").bind("keyup keypress pageload", function(e){
-  var variables = $("#variables").val() || "{}";
-  var template = $("#template").val();
-  if(!window.isFirst){
-	  window.isFirst = true;
-	  if(variables === sessionStorage.variables && template === sessionStorage.template){
-		return true;
-	}
-  }
-  if(e.target.id =="variables"){
-    try{
-        JSON.parse(variables);
-    }
-    catch(eee){
-      error();
-	  return true;
-    }
-  }
-  if(e.target.id =="template") {
-    if(template.split("{").length !== template.split("}").length) {
-      error();
-      return true;
-     }
-  }
-  $.post("render.php", {'template':template , 'variables' : variables } , 
-    function(d){
-	  $("pre").html(d).css("color","black");
-	  if("sessionStorage" in window){
-			sessionStorage.setItem("variables", variables);
-			sessionStorage.setItem("template", template);
-		}
-		$("#permalink").attr("href", function(){
-			return "/?" + $("form").serialize();
-		});
-		if("replaceState" in window.history){
-			history.replaceState({}, document.title, $("#permalink").attr("href"));
-		}
-    }).error(error);
+$(function() {
 
-});
-
-$(function(){
- if(template && variables){
-	$("#template").val(template);
-	$("#variables").val(variables);
-}
-else{
-	if("sessionStorage" in window){
-		for(var key in sessionStorage){
-			if(sessionStorage.hasOwnProperty(key)){
-				$("#"+key).val(sessionStorage[key]);
+	$("form").bind("keyup keypress pageload", function(e) {
+		var variables = $("#variables").val() || "{}";
+		var template = $("#template").val();
+		if (!window.isFirst) {
+			window.isFirst = true;
+			if (variables === sessionStorage.variables && template === sessionStorage.template) {
+				return true;
 			}
 		}
-	}
-}
-$("form").trigger("pageload");
-});
+		if (e.target.id == "variables") {
+			try {
+				JSON.parse(variables);
+			}
+			catch(eee) {
+				error();
+				return true;
+			}
+		}
+		if (e.target.id == "template") {
+			if (template.split("{").length !== template.split("}").length) {
+				error();
+				return true;
+			}
+		}
+		$.post("render.php", {
+			'template': template,
+			'variables': variables
+		},
+		function(d) {
+			$("pre").html(d).css("color", "black");
+			if ("sessionStorage" in window) {
+				sessionStorage.setItem("variables", variables);
+				sessionStorage.setItem("template", template);
+			}
+			$("#permalink").attr("href", function() {
+				return "/?" + $("form").serialize();
+			});
+			if ("replaceState" in window.history) {
+				history.replaceState({},
+				document.title, $("#permalink").attr("href"));
+			}
+		}).error(error);
 
+	});
+
+	$(function() {
+		if (template && variables) {
+			$("#template").val(template);
+			$("#variables").val(variables);
+		} else {
+			if ("sessionStorage" in window) {
+				for (var key in sessionStorage) {
+					if (sessionStorage.hasOwnProperty(key)) {
+						$("#" + key).val(sessionStorage[key]);
+					}
+				}
+			}
+		}
+		$("form").trigger("pageload");
+	});
 
 });
