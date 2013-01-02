af60bdd606156e8f6754cb8684815ae2c3c52d90
yahelc <yahelc@gmail.com>
2013-01-02 11:01:10 -0500

    Move inline styles to style block

--- index.php
+++ index.php
@@ -4,6 +4,19 @@
 	<title>Twig Playground</title>
 <script src ="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"> </script>
 <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css" rel="stylesheet">
+<style>
+#variables{ 
+  width: 240px; 
+  height:50px;
+}
+
+#template{
+  width: 640px;
+  height: 160px;
+}
+}
+</style>
+
 </head>
 <body>
 <h2>Twig Playground</h2>
@@ -12,10 +25,10 @@
 <form>
 <label for="variables">Variables JSON</label>
 
-<textarea id="variables" name="variables" style="width: 240px; height:50px;"></textarea>
+<textarea id="variables" name="variables"></textarea>
 <br><br>
 <label for="template">Template</label>
-<textarea id="template" name="template" style="width: 640px ;height: 160px"></textarea>
+<textarea id="template" name="template"></textarea>
 </form>
 <pre>
 
